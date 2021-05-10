1a875c284b0d4d1b5d011d6c5813e2b925359882
amoroabd <amoroabd@gmail.com>
2021-05-10 07:02:14 +0000

    routes imported to the app.js file

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ const adminRoutes = require("./src/controllers/commons/admin/commons");
 const userRoutes = require("./src/controllers/commons/user/commons");
 const routes = require("./src/controllers/routes/routes");
 const router = require("./src/controllers/commons/admin/commons");
+const adminRoute = require("./src/controllers/routes/adminRoute");
+const assessmentRoute = require("./src/controllers/routes/assessmentRoute");
+const courseRoute = require("./src/controllers/routes/courseRoute");
+const trackRoute = require("./src/controllers/routes/trackRoute");
+const traineeRoute = require("./src/controllers/routes/traineeRoute");
+
+// IMPORTING THE ROUTES
+const userRoute = require("./src/controllers/routes/userRoute");
+const adminRoute = require("./src/controllers/routes/adminRoute");
+const assessmentRoute = require("./src/controllers/routes/assessmentRoute");
+const courseRoute = require("./src/controllers/routes/courseRoute");
+const trackRoute = require("./src/controllers/routes/trackRoute");
+const traineeRoute = require("./src/controllers/routes/traineeRouteRoute");
 
 // Initialize app
 const app = express();
@@ -24,13 +37,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
 
+// USING THE ROUTES
+app.use("/", userRoute);
+app.use("/", adminRoute);
+app.use("/", assessmentRoute);
+app.use("/", courseRoute);
+app.use("/", trackRoute);
+app.use("/", traineeRoute);
 
-// Home Page route
+// HOME ROUTE
 app.get("/", function (req, res) {
   res.send("<h2> Home Page Route </h2>");
 });
 
-// Starting the server
+// STARTING THE SERVER
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
